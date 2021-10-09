bac21e01874331a39730c7d0d93da27564cf6665
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-09 21:53:36 +0700

    Getting new inserted value and text size

--- TagView/View/TagView.swift
+++ TagView/View/TagView.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct TagView: View {
     var maxLimit: Int
     @Binding var tags: [Tag]
+    
     var title: String = "Add some Tags"
+    var fontSize: CGFloat = 16
     
     var body: some View {
         VStack(alignment: .leading, spacing: 15) {
@@ -30,6 +32,22 @@ struct TagView: View {
                 RoundedRectangle(cornerRadius: 10)
                     .strokeBorder(Color.white.opacity(0.5), lineWidth: 1)
             )
+        }
+        .onChange(of: tags) { newValue in
+            // Getting new inserted value
+            guard let last = tags.last
+            else {
+                return
+            }
+            
+            // Getting text size
+            let font = UIFont.systemFont(ofSize: fontSize)
+            
+            let attributes = [NSAttributedString.Key.font: font]
+            
+            let size = (last.text as NSString).size(withAttributes: attributes)
+            
+            print(size)
         }
     }
 }
