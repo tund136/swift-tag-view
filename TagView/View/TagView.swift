aac84e9ce498c9edcb54e8ac4d35f742057f7b71
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-09 21:05:10 +0700

    Create Tag View

---
+++ TagView/View/TagView.swift
@@ -0,0 +1,18 @@
+//
+//  TagView.swift
+//  TagView
+//
+//  Created by Danh Tu on 09/10/2021.
+//
+
+import SwiftUI
+
+// Custom view
+struct TagView: View {
+    var maxLimit: Int
+    var tags: []
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
