3a13bb8e16b333d37f9e43712e0014273f58723d
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-09 20:37:55 +0700

    Create the project

---
+++ TagView/TagViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagViewApp.swift
+//  TagView
+//
+//  Created by Danh Tu on 09/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TagViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
