ece9ee5745a3658bebd9fedd1994a7e885dac137
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-09 20:40:34 +0700

    Setting up project

---
+++ TagView/View/Home.swift
@@ -0,0 +1,14 @@
+//
+//  Home.swift
+//  TagView
+//
+//  Created by Danh Tu on 09/10/2021.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
